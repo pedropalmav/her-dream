57c660d63b0a4a41c78c32266eac6152d85172bc
IgnAMS <iams.monardes@gmail.com>
2026-04-17 15:51:29 -0400

    fix: fixing task name

--- random_goal.sh
+++ random_goal.sh
@@ -13,6 +13,6 @@ export MUJOCO_GL=egl
 export OMP_NUM_THREADS=4
 export MKL_NUM_THREADS=4
 
-# ts -G 1 bash runs/random_goal.sh logdir=./goal_dreamer_with_text/01 env=random_goal seed=0 mission_text=False
+# ts -G 1 bash random_goal.sh logdir=./goal_dreamer_with_text/01 seed=0 +mission_text=False
 xvfb-run -a -s '-screen 0 1024x768x24 -ac +extension GLX +render -noreset' \
   $HOME/.local/bin/uv run train.py "$@"
